e7bbf97daaaab6862beb8da454afa2d30b276383
bhavesh02github <127826427+bhavesh02github@users.noreply.github.com>
2025-06-20 18:18:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
  
 resource "aws_iam_role" "example_role" {
-  name = "example-iam-role"
+  name = "iam-role"
  
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
